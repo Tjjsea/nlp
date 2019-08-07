97c0f101214f5578ef745fa5305bcc0d6ab8fb80
TSEA <2694603918@qq.com>
2019-08-07 15:31:19 +0800

    'data_process'

--- data_process.py
+++ data_process.py
@@ -3,9 +3,65 @@
 
 import json
 
-length=0
-with open('UD_English-EWT/en_ewt-ud-train.conllu',encoding='utf-8') as fin:
-    datas=fin.readlines()
+def extract(mode):
+    '''
+    从treebank中抽取数据，选取长度在4-36之间的句子
+    '''
+    filepath='UD_English-EWT/en_ewt-ud-'+mode+'.conllu'
+    outpath='data/'+mode+'.json'
+    datas=open(filepath,encoding='utf-8').readlines()
+    exdatas=[]
+    sentence={}
+    count=0
+    form,lemma,POStag,head,label=[],[],[],[],[]
+    
+    for line in datas:
+        line=line.strip()
+        if not line:
+            if count<=3 or count>=37:
+                continue
+            count=0
+            sentence['word']=form
+            sentence['lemma']=lemma
+            sentence['POStag']=POStag
+            sentence['head']=head
+            sentence['label']=label
+            exdatas.append(sentence)
+            sentence={}
+            form,lemma,POStag,head,label=[],[],[],[],[]
+            continue
+        if line[0]=='#':
+            continue
+        line=line.split('\t')
+        if line[0].isdigit()==False:
+            continue        
+        form.append(line[1])
+        lemma.append(line[2])
+        POStag.append(line[3])
+        try:
+            line[6]=int(line[6])
+            line[0]=int(line[0])
+        except:
+            print(line)
+        
+        if line[6]==0:
+            line[6]=line[0]-1
+        else:
+            line[6]-=1
+        head.append(line[6])
+        la=line[7].find(':')
+        if la!=-1:
+            line[7]=line[7][:la]            
+        label.append(line[7])
+        count+=1
+    with open(outpath,'w',encoding='utf-8') as fout:
+        json.dump(exdatas,fout)
+
+def GetWordict():
+    filepath='UD_English-EWT/en_ewt-ud-train.conllu'
+    datas=open(filepath,encoding='utf-8').readlines()
+    wordict={"UNK":0,"PAD":1,"NUM":2}
+    count=3
     for line in datas:
         line=line.strip()
         if not line:
@@ -13,8 +69,26 @@ with open('UD_English-EWT/en_ewt-ud-train.conllu',encoding='utf-8') as fin:
         if line[0]=='#':
             continue
         line=line.split('\t')
-        if not line[0].isdigit():
+        word=line[1]
+        if word.isdigit():
             continue
-        if int(line[0])>length:
-            length=int(line[0])
-    print(length)
+        if word not in wordict:
+            wordict[word]=count
+            count+=1
+    outpath='data/words.json'
+    json.dump(wordict,open(outpath,'w'))
+
+def GetChardict():
+    char={'un':0,"pd":1}
+    A=65
+    a=97
+    for i in range(26):
+        char[chr(A+i)]=i+2
+    for i in range(26):
+        char[chr(a+i)]=i+28
+    with open('data/char.json','w',encoding='utf-8') as fout:
+        json.dump(char,fout)
+
+extract('train')
+extract('dev')
+extract('test')
