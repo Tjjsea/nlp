60c113a211f018e0b88f4932c50d4a8229d085de
TSEA <2694603918@qq.com>
2019-08-07 17:46:22 +0800

    tagging component

--- JModel.py
+++ JModel.py
@@ -6,51 +6,56 @@ import tensorflow as tf
 from CLE import MST,GetScore
 
 class JModel():
-    def __init__(self,sequence_length,max_words_length,vocab_size,wordembedding_size,char_size,charembedding_size,char_biunits,POS_biunits,num_POS,POSembedding_size,parse_biunits,num_label,learning_rate):
-        self.input_word=tf.placeholder(tf.int32,[None,sequence_length],name='input_word')
-        self.input_character=tf.placeholder(tf.int32,[None,sequence_length,max_words_length],name='input_character')
-        self.input_tag=tf.placeholder(tf.int32,[None,sequence_length,num_POS],name='POS_tag')
-        self.input_arc=tf.placeholder(tf.int32,[None,sequence_length],name='arc')
-        self.input_arclabel=tf.placeholder(tf.int32,[None,sequence_length],name='arc label')
-        self.position=tf.placeholder(tf.float32,[None,sequence_length],name='context position')
+    def __init__(self,FLAGS):
+        self.input_word=tf.placeholder(tf.int32,[None,None],name='input_word')
+        self.input_character=tf.placeholder(tf.int32,[None,None,None],name='input_character')
+        self.input_tag=tf.placeholder(tf.int32,[None,None,None],name='POS_tag')
+        self.input_arc=tf.placeholder(tf.int32,[None,None],name='arc')
+        self.input_arclabel=tf.placeholder(tf.int32,[None,None],name='arc_label')
+        self.position=tf.placeholder(tf.float32,[None,None],name='context_position')
+        self.dropout_keep_prob=tf.placeholder(tf.float32,name='dropout_keep_prob')
 
         #word embeeding
-        with tf.name_scope("word embedding"):
-            self.word_embedding=tf.Variable(tf.random_uniform([vocab_size,wordembedding_size],-1.0,1.0))
+        with tf.name_scope("word_embedding"):
+            self.word_embedding=tf.Variable(tf.random_uniform([FLAGS.vocab_size,FLAGS.wordembedding_size],-1.0,1.0))
             self.word_embedded=tf.nn.embedding_lookup(self.word_embedding,self.input_word)  #[batch_size,sequence_length,wordembedding_size]
 
         #characer-level word embedding
-        with tf.name_scope("character-level word embedding"):
-            char_input=tf.reshape(self.input_character,[-1,max_words_length]) #
-            self.char_embedding=tf.Variable(tf.random_uniform([char_size,charembedding_size],-1.0,1.0))
+        with tf.name_scope("character-level_word_embedding"):
+            char_input=tf.reshape(self.input_character,[-1,FLAGS.max_words_length]) #
+            self.char_embedding=tf.Variable(tf.random_uniform([FLAGS.char_size,FLAGS.charembedding_size],-1.0,1.0))
             char_input=tf.nn.embedding_lookup(self.char_embedding,char_input) #[batch_size*sequence_length,max_words_length,charembedding_size]
             with tf.variable_scope("char-bilstm"):
-                fwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(char_biunits),output_keep_prob=self.dropout_keep_prob)
-                bwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(char_biunits),output_keep_prob=self.dropout_keep_prob)
+                fwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(FLAGS.char_biunits),output_keep_prob=self.dropout_keep_prob)
+                bwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(FLAGS.char_biunits),output_keep_prob=self.dropout_keep_prob)
 
-                outputs,_=tf.nn.bidirectional_dynamic_rnn(fwlstm,bwlstm,self.input_character)
+                outputs,_=tf.nn.bidirectional_dynamic_rnn(fwlstm,bwlstm,char_input,dtype=tf.float32)
                 charbioutput=tf.concat(outputs,2) #[batch_size*sequence_length,char_biunits*2]
-            charoutput=self.MLP(charbioutput,char_biunits*2,wordembedding_size,'char_embedding',tf.nn.leaky_relu)
-            self.char_embedded=tf.reshape([-1,sequence_length,wordembedding_size])
-            
-        self.embedded=tf.concat([self.word_embedded,self.char_embedded],2)   #将word-level与char-level的embedding拼接 [batch_size,sequence_length,2*wordembedding_size]
+            charoutput=self.MLP(charbioutput,FLAGS.char_biunits*2,FLAGS.wordembedding_size,'char_embedding',tf.nn.leaky_relu)
+            self.char_embedded=tf.reshape(charoutput,[-1,FLAGS.sequence_length,FLAGS.wordembedding_size])
+        
+        self.embedded=tf.concat([self.word_embedded,self.char_embedded],-1)   #将word-level与char-level的embedding拼接 [batch_size,sequence_length,2*wordembedding_size]
 
         #POS tagging component
-        with tf.name_scope("POS tagging component"):
+        with tf.name_scope("POS_tagging_component"):
+            self.position=tf.reshape(self.position,[FLAGS.batch_size,FLAGS.sequence_length,1])
             self.taginput=tf.concat([self.embedded,self.position],-1)  #拼接存在问题?
             with tf.name_scope("POS-bilstm"):
-                fwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(POS_biunits),output_keep_prob=self.dropout_keep_prob)
-                bwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(POS_biunits),output_keep_prob=self.dropout_keep_prob)
+                fwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(FLAGS.POS_biunits),output_keep_prob=self.dropout_keep_prob)
+                bwlstm=tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(FLAGS.POS_biunits),output_keep_prob=self.dropout_keep_prob)
 
-                outputs,_=tf.nn.bidirectional_dynamic_rnn(fwlstm,bwlstm,self.taginput)
+                outputs,_=tf.nn.bidirectional_dynamic_rnn(fwlstm,bwlstm,self.taginput,dtype=tf.float32)
             tagoutput=tf.concat(outputs,2)
-            self.tagout=self.MLP(tagoutput,POS_biunits*2,num_POS,'POS-tag',tf.nn.leaky_relu)
+            self.tagout=self.MLP(tagoutput,FLAGS.POS_biunits*2,FLAGS.num_POS,'POS-tag',tf.nn.leaky_relu)
             self.postag=tf.nn.softmax(self.tagout)#[batch_size,sequence_length,num_POS]
 
             self.loss1=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.input_tag,logits=self.postag))
             
         self.pos=tf.argmax(self.postag,-1)     #形状应为[batch_size,sequence_length,1] 预测的每个单词的标签
+        self.train1=tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(self.loss1)
+        self.saver=tf.train.Saver(tf.global_variables())
 
+        '''
         #Parsing component
         with tf.name_scope("parsing component"):
             POS_embedding=tf.Variable(tf.random_uniform([num_POS,POSembedding_size],-1.0,1.0))
@@ -101,6 +106,7 @@ class JModel():
             self.loss=tf.add(tf.add(self.loss1,self.loss2),self.loss3)
             self.train_op=tf.train.GradientDescentOptimizer(learning_rate).minimize(self.loss)
             self.saver = tf.train.Saver(tf.global_variables())
+        '''
         
     def MLP(self,inputs, insize, outsize, scope_name,activation_function=None):
         hidsize=128
@@ -114,9 +120,12 @@ class JModel():
                 out1=activation_function(out1)
             out2 = tf.matmul(out1,Weights2)+bias2
             return out2
-
-    def get_treescore(self,tree):
-        '''
-        获取句法树的评分
-        '''
-        pass
+        
+    def train(self,sess,batch):
+        feed_dict={self.input_word:batch.word,
+                   self.input_character:batch.char,
+                   self.input_tag:batch.tag,
+                   self.position:batch.position,
+                   self.dropout_keep_prob:0.5}
+        _,loss=sess.run([self.train1,self.loss1],feed_dict=feed_dict)
+        return loss
